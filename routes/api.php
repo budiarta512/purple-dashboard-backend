a469471d002b9a8283268d7caa93fc50450d3704
budiarta512 <budiarta5121@gmail.com>
2021-12-24 19:00:59 +0800

    new commit

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/test', function() {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json([
-        "test" => "success"
+        'status' => 'success',
+        'message' => 'Berhasil mengambil data',
+        'data' => [
+            'user' => $request->user()
+        ]
     ], 200);
 });
 
